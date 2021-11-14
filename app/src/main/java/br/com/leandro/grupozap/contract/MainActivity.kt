9960e270190fcc50eb2338199dae57fbf3799c17
Leandro <leandrodasilvalara@yahoo.com.br>
2021-11-14 12:35:34 -0300

    :memo: splash[#1]
    add

--- app/src/main/java/br/com/leandro/grupozap/contract/MainActivity.kt
+++ app/src/main/java/br/com/leandro/grupozap/contract/MainActivity.kt
@@ -1,11 +1,17 @@
-package br.com.leandro.grupozap
+package br.com.leandro.grupozap.contract
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.leandro.grupozap.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Thread.sleep(2000)
+
+        setTheme(R.style.Theme_Grupozap)
+
         setContentView(R.layout.activity_main)
     }
 }
