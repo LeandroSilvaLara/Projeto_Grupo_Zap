5bbd039c36f5c06e756fb0152b924a26639ee9c0
Leandro <leandrodasilvalara@yahoo.com.br>
2021-11-15 11:46:48 -0300

    :construction: add construção Building
    add

---
+++ app/src/main/java/br/com/leandro/grupozap/home/model/Building.kt
@@ -0,0 +1,43 @@
+package br.com.leandro.grupozap.home.model
+
+import java.io.Serializable
+
+data class Building (
+    var usableAreas: Int,
+    var listingType: String,
+    var createdAt: String,
+    var listingStatus: String,
+    var id: String,
+    var parkingSpaces: Int,
+    var updatedAt: String,
+    var owner: Boolean,
+    var images: ArrayList<String>,
+    var address: Address,
+    var bathrooms: Int,
+    var bedrooms: Int,
+    var pricingInfos: PricingInfos
+) : Serializable
+
+data class PricingInfos(
+    var yearlyIptu: String,
+    var price: String,
+    var businessType: String,
+    var monthlyCondoFee: String,
+
+    var period: String,
+    var rentalTotalPrice: String
+
+) : Serializable
+data class Address(
+    var city: String,
+    var neighborhood: String,
+    var geoLocation: Geolocation
+) : Serializable
+data class Geolocation(
+    var precision: String,
+    var location: Location
+) : Serializable
+data class Location(
+    var lon: Double,
+    var lat: Double
+) : Serializable
--- app/src/main/java/br/com/leandro/grupozap/home/view/BuildingFragment.kt
+++ app/src/main/java/br/com/leandro/grupozap/home/view/BuildingFragment.kt
@@ -1,4 +1,24 @@
 package br.com.leandro.grupozap.home.view
 
-class BuildingFragment {
+import android.content.Context
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import br.com.leandro.grupozap.home.model.Building
+
+class BuildingFragment (val items: List<Building>, val context: Context, val clickListener: (Building) -> Unit) :
+        RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
 }
