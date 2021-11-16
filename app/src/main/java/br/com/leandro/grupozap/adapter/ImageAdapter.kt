c4386783ec0cc8f3ff1c4ad7b88597398098573e
Leandro <leandrodasilvalara@yahoo.com.br>
2021-11-16 09:52:31 -0300

    :zap: definindo layout zap_activity
    add

--- app/src/main/java/br/com/leandro/grupozap/adapter/ImageAdapter.kt
+++ app/src/main/java/br/com/leandro/grupozap/adapter/ImageAdapter.kt
@@ -1,4 +1,35 @@
 package br.com.leandro.grupozap.adapter
 
-class ImageAdapter {
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import br.com.leandro.grupozap.R
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.imagens_itens.view.*
+
+class ImageAdapter (val items: List<String>, val context: Context) :
+        RecyclerView.Adapter<ImageAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(context).inflate(
+                R.layout.imagens_itens,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(parent: ImageAdapter.ViewHolder, viewType: Int) {
+        Glide.with(context).load(items.get(viewType)).into(parent?.ivBuildingImage);
+    }
+
+    override fun getItemCount(): Int {
+        return items.size
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val ivBuildingImage = view.photo_image_item
+    }
 }
